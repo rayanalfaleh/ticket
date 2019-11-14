2e297512dd73aa88aea4d14bddebfc4da8d7ff73
Rayanalfaleh <202392484@student.ttcollege.edu.sa>
2019-11-14 11:23:36 +0300

    Initial commit

---
+++ app/src/main/java/com/example/ticket/checkfares.java
@@ -0,0 +1,14 @@
+package com.example.ticket;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class checkfares extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checkfares);
+    }
+}
